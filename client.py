7fac357877af4d590c252a989d71ef9223a9c8bb
ysoto025 <55642058+ysoto025@users.noreply.github.com>
2020-09-20 14:30:55 -0400

    Create client.py

---
+++ client.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import sys
+import socket
+from sys import argv
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 2048)
+
+if len(argv) < 3:
+    print("missing arguments")
+    sys.exit()
+try:
+    sock.connect((argv[1], int(argv[2])))
+    print("It connected")
+except sock.gaierror as err:
+    print("Could not connect to host due error:" + err)
+
+file = open(argv[3], "rb")
+accio = ""
+
+sock.recv(5).decode("utf-8")
+
+sock.send(file)
+
+file.close()
+
+sock.close()
